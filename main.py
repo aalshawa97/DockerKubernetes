493ff7e032fd6166bc8d483309b69afc5067f885
Abdullah Mutaz Alshawa <abdullahalshawa@gmail.com>
2023-03-26 20:44:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Welcome")
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome'
+
+@app.route('/goodbye')
+def goodbye():
+    name = request.args.get('name')
+    return f'Goodbye, {name}!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
